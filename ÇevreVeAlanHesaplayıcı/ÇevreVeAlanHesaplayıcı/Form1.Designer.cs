08fde620f0a1a390e7e2fca501e20d606f0fc0c7
abdulkaharr <60811105+abdulkaharr@users.noreply.github.com>
2020-12-17 19:06:47 +0300

    ÇevreVeAlanHesaplayıcı

---
+++ ÇevreVeAlanHesaplayıcı/ÇevreVeAlanHesaplayıcı/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace ÇevreVeAlanHesaplayıcı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnkarealn = new System.Windows.Forms.Button();
+            this.btnkarecevre = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btndikaln = new System.Windows.Forms.Button();
+            this.btndikcevre = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("NewsGoth BT", 20.25F);
+            this.button1.Location = new System.Drawing.Point(51, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kare";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("NewsGoth BT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(331, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 70);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Dikdörtgen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(234, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(38, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kısa Kenar :";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(30, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 26);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Uzun Kenar :";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(171, 152);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(171, 201);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 29);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("NewsGoth BT", 20.25F);
+            this.button3.Location = new System.Drawing.Point(51, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 70);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Kare";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(171, 152);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 29);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox4.Location = new System.Drawing.Point(171, 201);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(124, 29);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.Visible = false;
+            // 
+            // btnkarealn
+            // 
+            this.btnkarealn.Font = new System.Drawing.Font("NewsGoth BT", 16F, System.Drawing.FontStyle.Bold);
+            this.btnkarealn.Location = new System.Drawing.Point(50, 247);
+            this.btnkarealn.Name = "btnkarealn";
+            this.btnkarealn.Size = new System.Drawing.Size(145, 64);
+            this.btnkarealn.TabIndex = 0;
+            this.btnkarealn.Text = "Alan Hesapla";
+            this.btnkarealn.UseVisualStyleBackColor = true;
+            this.btnkarealn.Visible = false;
+            this.btnkarealn.Click += new System.EventHandler(this.btnkarealn_Click);
+            // 
+            // btnkarecevre
+            // 
+            this.btnkarecevre.Font = new System.Drawing.Font("NewsGoth BT", 16F, System.Drawing.FontStyle.Bold);
+            this.btnkarecevre.Location = new System.Drawing.Point(201, 247);
+            this.btnkarecevre.Name = "btnkarecevre";
+            this.btnkarecevre.Size = new System.Drawing.Size(145, 64);
+            this.btnkarecevre.TabIndex = 0;
+            this.btnkarecevre.Text = "Çevre Hesapla";
+            this.btnkarecevre.UseVisualStyleBackColor = true;
+            this.btnkarecevre.Visible = false;
+            this.btnkarecevre.Click += new System.EventHandler(this.btnkarecevre_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(53, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 26);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Alan :";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(39, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 26);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Çevre :";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(127, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 26);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "00";
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(127, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 26);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "00";
+            this.label7.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(361, 139);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(201, 101);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sonuç";
+            this.groupBox1.Visible = false;
+            // 
+            // btndikaln
+            // 
+            this.btndikaln.Font = new System.Drawing.Font("NewsGoth BT", 16F, System.Drawing.FontStyle.Bold);
+            this.btndikaln.Location = new System.Drawing.Point(50, 247);
+            this.btndikaln.Name = "btndikaln";
+            this.btndikaln.Size = new System.Drawing.Size(145, 64);
+            this.btndikaln.TabIndex = 0;
+            this.btndikaln.Text = "Alan Hesapla";
+            this.btndikaln.UseVisualStyleBackColor = true;
+            this.btndikaln.Visible = false;
+            this.btndikaln.Click += new System.EventHandler(this.btndikaln_Click);
+            // 
+            // btndikcevre
+            // 
+            this.btndikcevre.Font = new System.Drawing.Font("NewsGoth BT", 16F, System.Drawing.FontStyle.Bold);
+            this.btndikcevre.Location = new System.Drawing.Point(201, 247);
+            this.btndikcevre.Name = "btndikcevre";
+            this.btndikcevre.Size = new System.Drawing.Size(145, 64);
+            this.btndikcevre.TabIndex = 0;
+            this.btndikcevre.Text = "Çevre Hesapla";
+            this.btndikcevre.UseVisualStyleBackColor = true;
+            this.btndikcevre.Visible = false;
+            this.btndikcevre.Click += new System.EventHandler(this.btndikcevre_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(594, 344);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btndikcevre);
+            this.Controls.Add(this.btnkarecevre);
+            this.Controls.Add(this.btndikaln);
+            this.Controls.Add(this.btnkarealn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " Hesaplayıcı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnkarealn;
+        private System.Windows.Forms.Button btnkarecevre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btndikaln;
+        private System.Windows.Forms.Button btndikcevre;
+    }
+}
+
---
+++ ÇevreVeAlanHesaplayıcı/ÇevreVeAlanHesaplayıcı/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ÇevreVeAlanHesaplayıcı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label1.Text = "Kare";
+            label2.Visible = true;
+            label2.Text = "Kenar :";
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            groupBox1.Visible = true;
+            textBox3.Visible = true;  
+            button2.Enabled = false;
+            btnkarealn.Visible = true;
+            btnkarecevre.Visible = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label1.Text = "Dikdörtgen";
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            groupBox1.Visible = true;
+            textBox4.Visible = true;
+            textBox3.Visible = true;
+            btndikcevre.Visible = true;
+            btndikaln.Visible = true;
+            button3.Enabled = false;
+        }
+
+        private void btnkarealn_Click(object sender, EventArgs e)
+        {
+            int kenar = 0, sonuc = 0 ;
+            kenar = Convert.ToInt32(textBox3.Text);
+            sonuc = kenar * kenar;
+            label6.Text = sonuc.ToString();
+           
+            
+
+        }
+
+        private void btnkarecevre_Click(object sender, EventArgs e)
+        {
+            int kenar = 0, sonuc = 0;
+            kenar = Convert.ToInt32(textBox3.Text);
+            sonuc = kenar * 4;
+            label7.Text = sonuc.ToString();
+
+        }
+
+        private void btndikaln_Click(object sender, EventArgs e)
+        {
+
+            int ukenar=0 , sonuc=0 ,kkenar=0;
+            ukenar = Convert.ToInt32(textBox3.Text);
+            kkenar = Convert.ToInt32(textBox4.Text);
+            sonuc = (ukenar) * (kkenar);
+            label6.Text = sonuc.ToString();
+            
+        }
+
+        private void btndikcevre_Click(object sender, EventArgs e)
+        {
+     
+            int ukenar = Convert.ToInt32(textBox3.Text);
+            int kkenar = Convert.ToInt32(textBox4.Text);
+            int sonuc = (ukenar + kkenar) * 2;
+            label7.Text = sonuc.ToString();
+        }
+    }
+}
